b9f59653a4a7f8d842dadd50f163130cb4da65c5
Alexander Sanchez <alexander.sanchez1.mt@correo.usa.edu.co>
2021-12-07 09:38:38 -0500

    Se crea el backend con la clase de servicio para usuarios

---
+++ Reto2/src/main/java/com/usa/ciclo4/serviceUser/ServiceUser.java
@@ -0,0 +1,161 @@
+package com.usa.ciclo4.serviceUser;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
+
+/**
+ * @author Alexander Sánchez Osorio
+ */
+@Service
+public class ServiceUser {
+
+    /**
+     * Atributo autowired para usuario
+     */
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * Agrega una ID automatico cuando se crea un usuario
+     * @return
+     */
+    private int getMaxID() {
+        List<User> users = userRepository.getAll();
+        ArrayList<Integer> ids = new ArrayList<>();
+        for (User user : users) {
+            ids.add(user.getId());
+
+        } Collections.sort(ids);
+        return ids.get(ids.size()-1) + 1;
+    }
+
+    // <READ - ASIGN NEW ID>
+    public int getId() {
+        for (int i=0; i<=getMaxID(); i++) {
+            Optional<User> exist = userRepository.getUserById(i);
+            if (exist.isEmpty()) {
+                return i;
+            }
+        } return -1;
+    }
+
+    /**
+     * Metodo para obtener todos los usuarios
+     * @return userRepository
+     */
+    public List<User> getAll(){
+        return userRepository.getAll();
+    }
+
+
+
+    public Optional getUserById(int id){
+        return userRepository.getUserById(id);
+    }
+
+    /**
+     * Guardar un usuario en la base de datos
+     * @param user
+     * @return user Devuelve el usuario actualizado.
+     */
+    public User save(User user){
+
+        List<User> users = userRepository.getAll();
+
+        if (users.size() == 0){
+            return userRepository.save(user);
+        }else if (getByEmail(user.getEmail()) == false) {
+            return userRepository.save(user);
+        }
+        return user;
+    }
+
+    /**
+     * Validar si un usuario existe en la base de datos
+     * @param email
+     * @return flag Devuelve el valor booleano actualizado
+     */
+    public boolean getByEmail(String email){
+
+        List<User> users = userRepository.getAll();
+        boolean flag = false;
+
+        for (User user: users) {
+            if(email.equals(user.getEmail())){
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * Autentificar si un usuario existe en la base de datos
+     * @param email
+     * @param pass
+     * @return noExiste Devuelve el valor indefinido cuando el usuario no existe en la base de datos
+     */
+    public User getByEmailPass(String email, String pass){
+        List<User> users = userRepository.getAll();
+        User noExiste = new User();
+
+        for (User user: users) {
+            if(email.equals(user.getEmail()) && pass.equals(user.getPassword())){
+                return user;
+            }
+        }
+        return noExiste;
+    }
+
+    /**
+     * Borrar un usuario por id en la base de datos
+     * @param idUser
+     */
+    public void delete (Integer idUser){
+        Optional<User> user = userRepository.getUserById(idUser);
+        if(user.isPresent()){
+            userRepository.deleteById(idUser);
+        }
+    }
+
+    /**
+     * Actualizar un usuario de la base de datos
+     * @param user
+     * @return
+     */
+    public User userUpdate(User user){
+        Optional<User> exist = userRepository.getUserById(user.getId());
+        if(exist.isPresent()) {
+            if (user.getIdentification() != null) {
+                exist.get().setIdentification(user.getIdentification());
+            }
+            if(user.getName() != null){
+                exist.get().setName(user.getName());
+            }
+            if(user.getAddress() != null){
+                exist.get().setAddress(user.getAddress());
+            }
+            if(user.getCellPhone() != null){
+                exist.get().setCellPhone(user.getCellPhone());
+            }
+            if(user.getEmail() != null){
+                exist.get().setEmail(user.getEmail());
+            }
+            if(user.getPassword() != null){
+                exist.get().setPassword(user.getPassword());
+            }
+            if(user.getZone() != null){
+                exist.get().setZone(user.getZone());
+            }
+            if(user.getType() != null){
+                exist.get().setType(user.getType());
+            }
+            return userRepository.save(exist.get());
+        }
+        return user;
+    }
+}
